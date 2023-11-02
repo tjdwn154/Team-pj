94f1dd599f59921147f6be4103eda60210a02cf6
moon <msj07199@gmail.com>
2023-11-02 11:48:13 +0900

    클라 배포

---
+++ src/components/Mypage/components/Coupon.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Coupon() {
+  return <p className="name2">쿠폰/상품권 등록</p>;
+}
+
+export default Coupon;
---
+++ src/components/Mypage/components/Qs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Qs = () => {
+  return <p className="name2">자주묻는질문</p>;
+};
+
+export default Qs;
